25336078717793933a20ad9fd043d296939d4049
Harvey Bitbucket <daves@harvest.org>
2025-05-15 16:12:38 -0700

    Initial commit

---
+++ src/main.js
@@ -0,0 +1,26 @@
+import { createAuth0Client } from '@auth0/auth0-spa-js';
+
+
+const loginBtn = document.getElementById('login');
+const output = document.getElementById('output');
+
+const show = (msg) => {
+  output.textContent += `\n${msg}`;
+};
+
+(async () => {
+  const auth0 = await createAuth0Client({
+    domain: 'dev-n6li42pa7lfgyazm.us.auth0.com',
+    client_id: 't2puMJ6IDe6LC8pKbV1ecG6N3mplbMAR',
+    audience: 'https://api.harvest.org',
+    cacheLocation: 'localstorage',
+    useRefreshTokens: true
+  });
+
+  loginBtn.onclick = async () => {
+    await auth0.loginWithPopup();
+    const user = await auth0.getUser();
+    show('✅ Logged in');
+    show(JSON.stringify(user, null, 2));
+  };
+})();
